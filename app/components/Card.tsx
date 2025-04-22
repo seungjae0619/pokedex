1f8c60f85e0e9ca0d0e761155b06ca305e76e14e
Park Seungjae <andrewpark0619@gmail.com>
2025-04-22 22:29:42 +0900

    update file structure

---
+++ app/components/Card.tsx
@@ -0,0 +1,3 @@
+export default function Card({ pokemon }) {
+  return <div className="flex justify-center border-2 rounded-2xl">card</div>;
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+export default function Header() {
+  return (
+    <div className="flex justify-center p-4">
+      <Image
+        className="mt-[-5]"
+        src="/pokeball.webp"
+        alt="pokéball"
+        width={50}
+        height={50}
+      />
+      <h2 className=" text-4xl text-transparent bg-clip-text text-2xl font-bold bg-gradient-to-r from-orange-300 to-red-600  ">
+        Pokédex
+      </h2>
+    </div>
+  );
+}
